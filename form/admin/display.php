e7ade5d9a5745ed8e6bdb78c50d3aed54137ca7b
parveensuyan <parveensuyan@gmail.com>
2022-03-31 07:12:47 -0700

    delete record

--- form/admin/display.php
+++ form/admin/display.php
@@ -10,37 +10,13 @@ $array_result = FetchValue();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Record</title>
     <link rel="stylesheet" href="index.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="index.js"></script>
 </head>
 <body>
 <table id="customers">
-
-<?php if(!empty($array_result)) {?>
-<tr>
- <th>Name</th>
- <th>Email</th>
- <th>Mobile</th>
- <th>Message</th>
- <th>Action</th>
-</tr>
-
-<?php 
-// print_r(1234);
-
-foreach($array_result as $value){?>
-<tr>
- <td><?php echo $value['name'];?></td>
- <td><?php echo $value['email'];?></td>
- <td><?php echo $value['mobile'];?></td>
- <td><?php echo $value['message'];?></td>
- <td>
-     <a href="edit.php/?id=<?php echo $value['id'];?>">Edit</a> | <a class="delete-anchor-nn"
-   href="#">Delete</a></td>
-</tr>
-<?php } 
-}
-else{ ?>
-    <p>NO RECORD FOUND</p>
-<?php }?></table>
+<?php include "index_table.php"; ?>
+</table>
 </body>
 </html>
 <?php
